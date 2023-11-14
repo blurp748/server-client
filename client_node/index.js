8902267e44b8b6a89a07e5a02c1e553d51460b42
Paul Delifer <paul.delifer@yahoo.fr>
2023-11-14 16:26:34 +0100

    Update index.js

--- client_node/index.js
+++ client_node/index.js
@@ -1,45 +1,36 @@
 const tls = require('tls');
-const net = require('net');
-
-const port = 8888;
-
-//Create the socket client.
-const client = new net.Socket();
-
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout,
 });
 
-//Connect to the server on the configured port 
-client.connect(port,function(){
-	//Log when the connection is established
-    console.log(`Connected to server on port ${port}`);
-    
-    const options = {
-        host: 'www.tanghe.fr',
-        port: port,
-        rejectUnauthorized: false
-    };
+const port = 8888;
 
-    const secureSocket = tls.connect(options, () => {
-        console.log(`TLS connection established with server on port ${port}`);
-        readline.question(`Calcul ? Write 0 to exit \n`, calcul => {
-            secureSocket.write(calcul);
-            readline.close();
-        });
-    });
+const options = {
+    host: 'www.tanghe.fr',
+    port: port,
+    rejectUnauthorized: false // Note: For testing purposes; use a valid certificate in production
+};
 
-    //Handle data coming from the server
-    secureSocket.on('data',function(data){
-        console.log(`Résultat : ${data}`);
+const secureSocket = tls.connect(options, () => {
+    console.log(`TLS connection established with server on port ${port}`);
+    readline.question(`Enter calculation. Write '0' to exit.\n`, calculation => {
+        secureSocket.write(calculation);
+        if (calculation === '0') {
+            secureSocket.destroy();
+        }
+        readline.close();
     });
-    // Handle connection close 
-    secureSocket.on('close',function(){
-        console.log('Connection Closed');
-    });
-    //Handle error
-    secureSocket.on('error',function(error){
-        console.error(`Connection Error ${error}`); 
-    });
+});
+
+secureSocket.on('data', function (data) {
+    console.log(`Result: ${data}`);
+});
+
+secureSocket.on('close', function () {
+    console.log('Connection Closed');
+});
+
+secureSocket.on('error', function (error) {
+    console.error(`Connection Error ${error}`);
 });
