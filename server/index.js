ebf632be2ee60e807804d3de5d2d9317a7a70162
Paul Delifer <paul.delifer@yahoo.fr>
2023-11-14 16:26:00 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const https = require('https');
+const tls = require('tls');
 const fs = require('fs');
 
 const options = {
@@ -8,66 +8,44 @@ const options = {
 
 const port = 8888;
 
-const server = https.createServer(options, onClientConnection);
+const server = tls.createServer(options, onClientConnection);
 
-server.listen(port,function(){
-   console.log(`Server started on port ${port}`); 
+server.listen(port, function () {
+    console.log(`Server started on port ${port}`);
 });
 
-var clients = [];
-
-//the client handling callback
-function onClientConnection(socket){
-    //Log when a client connnects.
+function onClientConnection(socket) {
     console.log(`${socket.remotePort} is connected`);
 
-    // Identify this client
-    socket.name = socket.remotePort
+    socket.name = socket.remotePort;
 
-    // Put this new client in the list
-    clients.push(socket);
-    
-	//Handle the client data.
-    socket.on('data',function(data){
-        //Log data received from the client
-        console.log(`Calcul : ${data}  de `+ socket.name);
-        
-        if(data == "0"){
+    socket.on('data', function (data) {
+        console.log(`Calculation: ${data} from ${socket.name}`);
+
+        if (data.toString().trim() === "0") {
             socket.end();
         }
 
-        var result = calculator(data);
-		
-		//prepare and send a response to the client 
-		socket.write(result);
-	});
-    
-	//Handle when client connection is closed
-    socket.on('close',function(){
-        console.log(`${socket.remoteAddress}:${socket.remotePort} Connection closed`);
-        clients.splice(clients.indexOf(socket), 1);
+        const result = calculator(data);
+        socket.write(result);
     });
-    
-	//Handle Client connection error.
-    socket.on('error',function(error){
+
+    socket.on('close', function () {
+        console.log(`${socket.remoteAddress}:${socket.remotePort} Connection closed`);
+    });
+
+    socket.on('error', function (error) {
         console.error(`${socket.remoteAddress}:${socket.remotePort} Connection Error ${error}`);
     });
-};
-
-function calculator(fn) {
-    var res;
-    try {
-        var res = new Function('return ' + fn)();
-        res = res.toString();
-    } catch (error) {
-        res = "MAUVAIS FORMAT";
-    }
-    return res;
 }
 
-function broadcast(sender){
-    clients.forEach(client => {
-        if (client === sender) return;
-        client.write("Everybody ! " + sender.name + " joined the party")
-    });
+function calculator(fn) {
+    let res;
+    try {
+        res = new Function('return ' + fn)();
+        res = res.toString();
+    } catch (error) {
+        res = "INVALID FORMAT";
+    }
+    return res;
 }
